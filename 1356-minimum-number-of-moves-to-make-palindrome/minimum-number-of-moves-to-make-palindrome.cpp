f9d56c2f91b7a8923060e66358905af12d5e9e5e
Adwaith R Krishna <adwaithrk19@gmail.com>
2025-06-22 23:48:33 +0530

    Time: 219 ms (5.25%) | Memory: 9.5 MB (88.34%) - LeetSync

---
+++ 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int minMovesToMakePalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+        int moves = 0;
+
+        while(i < j)
+        {
+            
+            if(s[i] != s[j])
+            {
+                bool found_element = false;   
+                for(int k = j; k > i; k--)
+                {
+                    if(s[k] == s[i])
+                    {
+                        found_element = true;
+                        cout << "found at : " << k << "\n";
+                        cout << s[k] << " == " << s[i] << "\n";
+                        for(int m = k; m < j; m++)
+                        {
+                            swap(s[m], s[m+1]);
+                            moves++;
+                        }
+                        j--;
+                        break;
+                    }
+                }
+
+                if(!found_element)
+                {
+                    cout << "\nelement not found : " << s[i] << "\n\n"; 
+                    moves += (s.size()/2 - i);
+                }
+                
+                i++;
+            }
+            else{
+                i++, j--;
+            }
+        }
+
+        return moves;        
+    }
+};
