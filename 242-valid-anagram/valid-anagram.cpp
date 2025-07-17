044fed9ae17003a2b54e2d95b05adf4241db7c62
Adwaith R Krishna <adwaithrk19@gmail.com>
2025-07-17 09:48:12 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (11.81%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,28 +4,19 @@ public:
         string max_string, min_string;
         unordered_map<char, int> character_map; 
 
-        if(s.size() > t.size())
-        {
-            max_string = s;
-            min_string = t;
-        }
-        else
-        {
-            max_string = t;
-            min_string = s;
-        }
+        if(s.size() != t.size()) return false;
 
 
-        for(int i = 0; i < min_string.size(); i++)
+        for(int i = 0; i < s.size(); i++)
         {
-            character_map[min_string[i]]++;
+            character_map[s[i]]++;
         }
 
-        for(int i = 0; i < max_string.size(); i++)
+        for(int i = 0; i < t.size(); i++)
         {
-            if(character_map[max_string[i]] > 0)
+            if(character_map[t[i]] > 0)
             {
-                character_map[max_string[i]]--;
+                character_map[t[i]]--;
             }
             else return false;
         }
