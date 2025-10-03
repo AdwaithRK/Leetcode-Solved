5cd360686b45fa95c8d164f24faf36590a1695fa
Adwaith R Krishna <adwaithrk19@gmail.com>
2025-10-03 07:26:13 +0530

    Time: 11 ms (42.31%) | Memory: 47.1 MB (9.86%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        vector<int> temp_greed = g;
+        vector<int> temp_size = s;
+
+        sort(temp_greed.begin(), temp_greed.end());
+        sort(temp_size.begin(), temp_size.end());
+
+        int greed_index = 0;
+        int size_index = 0;
+        int count = 0;
+
+        while(size_index < temp_size.size() && greed_index < temp_greed.size()){
+
+            if(temp_size[size_index] >= temp_greed[greed_index]){
+                greed_index++;
+                count++;
+            }
+
+            size_index++;
+        }
+        
+        return count;
+    }
+};
