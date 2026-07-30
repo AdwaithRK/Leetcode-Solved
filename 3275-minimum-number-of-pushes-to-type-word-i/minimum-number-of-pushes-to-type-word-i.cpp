c6d52c0f9d00bb7e30c212991285a035be256ffb
Adwaith R Krishna <adwaithrk19@gmail.com>
2026-07-30 10:11:20 +0530

    Time: 7 ms (1.39%) | Memory: 8.4 MB (54.67%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+
+        int size = word.size();
+        int press = 1, count = 0;
+        while(size > 0){
+            cout << "press : " << press << " size : " << size << "\n";
+            if(size > 8){
+                count += (press * 8);
+                size = size - 8;
+            }
+            else {
+                count += (press * size);
+                size = 0;
+            }
+            press++;
+        }
+        
+        return count;
+    }
+};
