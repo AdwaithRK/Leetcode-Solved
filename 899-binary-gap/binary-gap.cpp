93d3bf0c127674f8f5c491861d43ab2628bd6b3e
Adwaith R Krishna <adwaithrk19@gmail.com>
2026-07-31 23:16:07 +0530

    Time: 3 ms (1.70%) | Memory: 7.9 MB (68.29%) - LeetSync

---
+++ 899-binary-gap/binary-gap.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        bool found_one = false;
+        int count = 0;
+        int ans = 0;
+        while(n != 0) {
+
+            printf("%b \n", n);
+
+            if(found_one)
+                count++;
+            
+            if(n & 1) {
+                
+                if(found_one){
+                    ans = max(ans, count);
+                    count = 0;
+                }
+                else{
+                    found_one = true;
+                }
+            }
+
+            n = n >> 1;
+        }
+        
+        return ans;
+    }
+};
