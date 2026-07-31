025707b2b08dd060935f83e9c9f5b08bb38d8da6
Adwaith R Krishna <adwaithrk19@gmail.com>
2026-08-01 00:10:44 +0530

    Time: 40 ms (79.48%) | Memory: 33.9 MB (85.18%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.cpp
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
-        
-        vector<int> map(26, 0);
-        int false_count = 0;
-        for(auto character : allowed) map[character - 'a'] = 1;
 
-        for(int i = 0; i < words.size(); i++){
-            for(int j = 0; j < words[i].size(); j++){
-                if(!map[words[i][j] - 'a']){ 
-                    false_count++;
-                    break; 
-                }
+        int allowedMask = 0, wordMask = 0, ans = 0;
+
+        for(auto character : allowed){
+            allowedMask |= (1 << (character - 'a') );
+        }
+
+
+        for(auto word : words){
+            wordMask = 0;
+            for(auto character : word){
+                wordMask |= (1 << (character - 'a') ); 
             }
+
+            if( (wordMask & allowedMask) == wordMask) ans++;
         }
         
-        return words.size() - false_count;
+        return ans;
     }
 };
