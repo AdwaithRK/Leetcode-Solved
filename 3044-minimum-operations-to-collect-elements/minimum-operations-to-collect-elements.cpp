0406119715ba7cc03185fc9df7915143cf7629a9
Adwaith R Krishna <adwaithrk19@gmail.com>
2026-08-02 14:30:51 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (96.98%) - LeetSync

--- 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.cpp
+++ 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.cpp
@@ -6,7 +6,6 @@ public:
 
         for(int i = nums.size() -1; i>= 0; i--){
             if(nums[i] <= k){ 
-                //cout << "setting : " << i << "\n";
                 mapi.set(nums[i]); 
             }
 
